b63e483245331f27098d6618c75420b617d4bc7e
Irsyad Ashari <irsyad.ashari17@gmail.com>
2024-05-21 14:46:15 +0800

    Initial Commit, creating project

---
+++ MyPokemonList/MyPokemonList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPokemonList
+//
+//  Created by Irsyad Ashari on 21/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
